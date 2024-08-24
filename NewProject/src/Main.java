8e78470ccee6f73a09676ef5e44950a0ebcb9c4c
unknown <goliveiramisael18@gmail.com>
2024-08-23 22:32:17 -0300

    ProjetoJava

--- NewProject/src/Main.java
+++ NewProject/src/Main.java
@@ -5,21 +5,33 @@ public class Main {
         int [] list1 = {4,5,6,12,22,41,54};
         int [] list2 = {20,29,33,41,12,10};
 
-        int[] listanova = new int[30];
-        int unir = 0;
+        int[] listanova = new int[list1.length + list2.length];
 
-        for (int i = 0; i < list1.length; i++) {
-            listanova[unir++] = list1[i];
+        int i = 0;
+
+        while (i < list1.length){
+            listanova[i] = list1[i];
+            i++;
+
         }
 
-        for (int i = 0; i < list2.length; i++) {
-            listanova[unir++] = list2[i];
+        i = list1.length;
+        int j = 0;
+
+        while (j < list2.length){
+            listanova[j + i] = list2[j];
+            j++;
         }
 
 
-        for (int i = 0; i < listanova.length; i++) {
-            System.out.print(listanova[i] + " ");
-        }
+        System.out.println("Mostrando a lista nova");
+
+        for (int k = 0; k < listanova.length; k++)
+            System.out.println("Lista nova posição " + k + " - " + listanova[k]);
+
+
+
+
 
 
         }
